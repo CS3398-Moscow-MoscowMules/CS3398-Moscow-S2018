bbcdc4299299828b8fda32fba319eac4297ab4d1
RebeccaYCario <35672019+RebeccaYCario@users.noreply.github.com>
2018-02-24 18:44:28 -0600

    More communication between model and controller
    Passed information from database to playerCharacter

--- Dnd5eCharacterSheet/app/src/main/java/jones/scott/dnd5echaractersheet/Class.java
+++ Dnd5eCharacterSheet/app/src/main/java/jones/scott/dnd5echaractersheet/Class.java
@@ -4,6 +4,9 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.ColumnInfo;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by HEYSCOTT on 2/15/18.
  */
@@ -18,9 +21,17 @@ public class Class {
     @ColumnInfo(name = "class")
     private String className;
 
-    public Class(int cid, String className) {
+    @ColumnInfo(name = "health")
+    private int health;
+
+    @ColumnInfo(name = "skills")
+    private List<Boolean> skills;
+
+    public Class(int cid, String className, int health, List<Boolean> skills) {
         this.cid = cid;
         this.className = className;
+        this.health = health;
+        this.skills = skills;
     }
 
     public int getCid() {
@@ -39,20 +50,22 @@ public class Class {
         this.className = className;
     }
 
+    public int getHealth() { return health; }
+
     public static Class[] populatedData() {
         return new Class[] {
-                new Class(1, "Barbarian"),
-                new Class(2, "Bard"),
-                new Class(3, "Cleric"),
-                new Class(4, "Druid"),
-                new Class(5, "Fighter"),
-                new Class(6, "Monk"),
-                new Class(7, "Paladin"),
-                new Class(8, "Ranger"),
-                new Class(9, "Rogue"),
-                new Class(10, "Sorcerer"),
-                new Class(11, "Warlock"),
-                new Class(12, "Wizard")
+                new Class(1, "Barbarian", 12, Arrays.asList(false, true, false, true, false, false, false, true, false, false, true, true, false, false, false, false, false, true)),
+                new Class(2, "Bard", 8, Arrays.asList(true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true)),
+                new Class(3, "Cleric", 8, Arrays.asList(false, false, false, false, false, true, true, false, false, true, false, false, false, true, true, false, false, false)),
+                new Class(4, "Druid", 8, Arrays.asList(true, true, false, true, false, true, true, true, false, false, false, true, false, false, false, false, false, true)),
+                new Class(5, "Fighter", 10, Arrays.asList(true, true, false, true, false, true, true, true, false, false, false, true, false, false, false, false, false, true)),
+                new Class(6, "Monk", 8, Arrays.asList(true, false, false, true, false, true, true, false, false, false, false, false, false, false, true, false, true, false)),
+                new Class(7, "Paladin", 10, Arrays.asList(false, false, false, true, false, false, true, true, false, true, false, false, false, true, true, false, false, false)),
+                new Class(8, "Ranger", 10, Arrays.asList(false, true, false, true, false, false, true, false, true, false, true, true, false, false, false, false, true, true)),
+                new Class(9, "Rogue", 8, Arrays.asList(true, false, false, true, true, false, true, true, true, false, false, true, true, true, false, true, true, false)),
+                new Class(10, "Sorcerer", 6, Arrays.asList(false, false, true, false, true, false, true, true, false, false, false, false, false, true, true, false, false, false)),
+                new Class(11, "Warlock", 8, Arrays.asList(false, false, true, false, true, true, false, true, true, false, true, false, false, false, true, false, false, false)),
+                new Class(12, "Wizard", 6, Arrays.asList(false, false, true, false, false, true, true, false, true, true, false, false, false, false, true, false, false, false))
         };
 
     }

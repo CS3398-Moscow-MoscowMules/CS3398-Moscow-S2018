9b6bd43286c642696b4d2ea4802762d3d7e841d3
mikesal318 <35582687+mikesal318@users.noreply.github.com>
2018-04-04 16:09:19 -0500

    Updating Create character java file.

--- MyApplication/app/src/main/java/mds158/charactersheet/com/myapplication/CreateCharacterActivity.java
+++ MyApplication/app/src/main/java/mds158/charactersheet/com/myapplication/CreateCharacterActivity.java
@@ -2,83 +2,89 @@ package mds158.charactersheet.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.view.View;
-import android.widget.AdapterView;
-import java.util.ArrayList;
-import java.util.List;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemSelectedListener;
 
-public class CreateCharacterActivity extends AppCompatActivity {
+public class CreateCharacterActivity extends AppCompatActivity implements
+        AdapterView.setOnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_character);
 
-        //******TEAM, This code is still being finnicky, Payton and I are working on a solution.
-        //
-        /*
-        Spinner race = (Spinner) findViewById(R.id.race);
+        Spinner race = findViewById(R.id.race);
         race.setOnItemSelectedListener(this);
-        ArrayAdapter<String> raceAdapter = ArrayAdapter.createFromResource(this,
+        ArrayAdapter<CharSequence> raceAdapter = ArrayAdapter.createFromResource(this,
                 R.array.race_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        raceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         race.setAdapter(raceAdapter);
-        Spinner background = (Spinner) findViewById(R.id.background);
-        background.setOnItemSelectedListener(this);
-        ArrayAdapter<String> backgroundAdapter = ArrayAdapter.createFromResource(this,
-                R.array.background_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        background.setAdapter(backgroundAdapter);
-        Spinner alignment = (Spinner) findViewById(R.id.alignment);
-        alignment.setOnItemSelectedListener(this);
-        ArrayAdapter<String> alignmentAdapter = ArrayAdapter.createFromResource(this,
-                R.array.alignment_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        alignment.setAdapter(alignmentAdapter);
-        Spinner language = (Spinner) findViewById(R.id.Language);
-        language.setOnItemSelectedListener(this);
-        ArrayAdapter<String> languageAdapter = ArrayAdapter.createFromResource(this,
-                R.array.language_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        language.setAdapter(languageAdapter);
-        Spinner armor = (Spinner) findViewById(R.id.armor_spinner);
-        armor.setOnItemSelectedListener(this);
-        ArrayAdapter<String> armorAdapter = ArrayAdapter.createFromResource(this,
-                R.array.armor_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        armor.setAdapter(armorAdapter);
-        Spinner shield = (Spinner) findViewById(R.id.shield_spinner);
-        shield.setOnItemSelectedListener(this);
-        ArrayAdapter<String> shieldAdapter = ArrayAdapter.createFromResource(this,
-                R.array.shield_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        shield.setAdapter(shieldAdapter);
-        Spinner spell = (Spinner) findViewById(R.id.spells);
+
+        Spinner spell = findViewById(R.id.spells);
         spell.setOnItemSelectedListener(this);
-        ArrayAdapter<String> spellAdapter = ArrayAdapter.createFromResource(this,
+        ArrayAdapter<CharSequence> spellAdapter = ArrayAdapter.createFromResource(this,
                 R.array.spells_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spellAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spell.setAdapter(spellAdapter);
-        Spinner weapon = (Spinner) findViewById(R.id.weapons_spinner);
-        weapon.setOnItemSelectedListener(this);
-        ArrayAdapter<String> weaponAdapter = ArrayAdapter.createFromResource(this,
-                R.array.weapons_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        weapon.setAdapter(weaponAdapter);
-        Spinner class = (Spinner) findViewById(R.id.classification);
-        class.setOnItemSelectedListener(this);
-        ArrayAdapter<String> classAdapter = ArrayAdapter.createFromResource(this,
+
+        Spinner classes = findViewById(R.id.classification);
+        classes.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> classAdapter = ArrayAdapter.createFromResource(this,
                 R.array.class_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        class.setAdapter(classAdapter);
-        */
+        classAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        classes.setAdapter(classAdapter);
+
+        Spinner weapon = findViewById(R.id.weapons_spinner);
+        weapon.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> weaponAdapter = ArrayAdapter.createFromResource(this,
+                R.array.weapons_arrays, android.R.layout.simple_spinner_item);
+        weaponAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        weapon.setAdapter(weaponAdapter);
+
+        Spinner shield = findViewById(R.id.shield_spinner);
+        shield.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> shieldAdapter = ArrayAdapter.createFromResource(this,
+                R.array.shield_arrays, android.R.layout.simple_spinner_item);
+        shieldAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        shield.setAdapter(shieldAdapter);
+
+        Spinner armor = findViewById(R.id.armor_spinner);
+        armor.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> armorAdapter = ArrayAdapter.createFromResource(this,
+                R.array.armor_arrays, android.R.layout.simple_spinner_item);
+        armorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        armor.setAdapter(armorAdapter);
+
+        Spinner language = findViewById(R.id.Language);
+        language.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> languageAdapter = ArrayAdapter.createFromResource(this,
+                R.array.language_arrays, android.R.layout.simple_spinner_item);
+        languageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        language.setAdapter(languageAdapter);
+
+        Spinner alignment = findViewById(R.id.alignment);
+        alignment.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> alignmentAdapter = ArrayAdapter.createFromResource(this,
+                R.array.alignment_arrays, android.R.layout.simple_spinner_item);
+        alignmentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        alignment.setAdapter(alignmentAdapter);
+
+        Spinner background = findViewById(R.id.background);
+        background.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> backgroundAdapter = ArrayAdapter.createFromResource(this,
+                R.array.background_arrays, android.R.layout.simple_spinner_item);
+        backgroundAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        background.setAdapter(backgroundAdapter);
+
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+        String item = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent){}
 }
